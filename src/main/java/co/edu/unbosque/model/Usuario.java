8ae206633e89653aad837a9724f1ec147a51d16f
ccarlos19 <57632969+ccarlos19@users.noreply.github.com>
2021-11-08 11:30:44 -0500

    Agrega usuario

--- src/main/java/co/edu/unbosque/model/Usuario.java
+++ src/main/java/co/edu/unbosque/model/Usuario.java
@@ -1,4 +1,5 @@
 package co.edu.unbosque.model;
+
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -10,28 +11,25 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "usuario")
+@Table(name = "usuario")
 public class Usuario {
-	
-	  @Id
-	  	private long id;
-		private String clave;
-		private int edad;
-		private Date fechaNacimiento;
-		private String nombre;
-		private String apellido;
-		private String rol;
-	
-	
-	
-	
-	public Usuario () {
-		
-	}
-	
-	
 
-	public Usuario(long id, String clave, int edad, Date fechaNacimiento, String nombre, String apellido, String rol) {
+	@Id
+	private long id;
+	private String clave;
+	private int edad;
+	private Date fechaNacimiento;
+	private String nombre;
+	private String apellido;
+	private String rol;
+	private String usuario;
+
+	public Usuario() {
+
+	}
+
+	public Usuario(long id, String clave, int edad, Date fechaNacimiento, String nombre, String apellido, String rol,
+			String usuario) {
 		super();
 		this.id = id;
 		this.clave = clave;
@@ -40,90 +38,67 @@ public class Usuario {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.rol = rol;
+		this.usuario = usuario;
 	}
-
-
-
-
-
-
 
 	public String getApellido() {
 		return apellido;
 	}
 
-
-
-
-
-
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
-
-
-
-
-
-
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
-
-
 
 	public String getClave() {
 		return clave;
 	}
 
-
-
 	public void setClave(String clave) {
 		this.clave = clave;
 	}
-
-
 
 	public int getEdad() {
 		return edad;
 	}
 
-
-
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
-
 
 	public Date getFechaNacimiento() {
 		return fechaNacimiento;
 	}
 
-
-
 	public void setFechaNacimiento(Date fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
-
-
 
 	public String getRol() {
 		return rol;
 	}
 
-
-
 	public void setRol(String rol) {
 		this.rol = rol;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
 	}
 	
 }
