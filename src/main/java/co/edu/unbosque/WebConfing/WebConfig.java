d1d187887ddf1e2003da9d2a8127463764ff8764
Charli199 <61799577+Charli199@users.noreply.github.com>
2021-11-10 18:07:44 -0500

    Update WebConfig.java

--- src/main/java/co/edu/unbosque/WebConfing/WebConfig.java
+++ src/main/java/co/edu/unbosque/WebConfing/WebConfig.java
@@ -38,14 +38,14 @@ public class WebConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	
-    	/*
+    	
         http
           .authorizeRequests()          
           .anyRequest()          
           .authenticated()
           .and()
           .httpBasic();
-          */
+          
     }
     	
 
