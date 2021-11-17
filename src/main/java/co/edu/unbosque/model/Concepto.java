89e98482ce6dfb268844e7b72a2bb00b60d74c22
Juan Pablo Velasquez Camargo <61720010+JP-Velasquez@users.noreply.github.com>
2021-11-17 14:59:09 -0500

    Persistencia

--- src/main/java/co/edu/unbosque/model/Concepto.java
+++ src/main/java/co/edu/unbosque/model/Concepto.java
@@ -1,5 +1,42 @@
 package co.edu.unbosque.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "concepto")
 public class Concepto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name = "concepto", nullable = false)
+	private String concepto;
+
+	public Concepto(long id, String concepto) {
+		super();
+		this.id = id;
+		this.concepto = concepto;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getConcepto() {
+		return concepto;
+	}
+
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
 
 }
--- src/main/java/co/edu/unbosque/model/Presupuesto.java
+++ src/main/java/co/edu/unbosque/model/Presupuesto.java
@@ -1,5 +1,111 @@
 package co.edu.unbosque.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "presupuesto")
 public class Presupuesto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name = "id_categoria", nullable = false)
+	private long id_categoria;
+	@Column(name = "id_concepto", nullable = false)
+	private long id_concepto;
+	@Column(name = "anio", nullable = false)
+	private Date anio;
+	@Column(name = "ppto_asignado", nullable = false)
+	private double ppto_asignado;
+	@Column(name = "porce_ppto_alcanzado", nullable = false)
+	private double porce_ppto_alcanzado;
+	@Column(name = "ppto_alcanzado")
+	private double ppto_alcanzado;
+	@Column(name = "ppto_restante", nullable = false)
+	private double ppto_restante;
+
+	public Presupuesto(long id, long id_categoria, long id_concepto, Date anio, double ppto_asignado,
+			double porce_ppto_alcanzado, double ppto_alcanzado, double ppto_restante) {
+		super();
+		this.id = id;
+		this.id_categoria = id_categoria;
+		this.id_concepto = id_concepto;
+		this.anio = anio;
+		this.ppto_asignado = ppto_asignado;
+		this.porce_ppto_alcanzado = porce_ppto_alcanzado;
+		this.ppto_alcanzado = ppto_alcanzado;
+		this.ppto_restante = ppto_restante;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(long id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+	public long getId_concepto() {
+		return id_concepto;
+	}
+
+	public void setId_concepto(long id_concepto) {
+		this.id_concepto = id_concepto;
+	}
+
+	public Date getAnio() {
+		return anio;
+	}
+
+	public void setAnio(Date anio) {
+		this.anio = anio;
+	}
+
+	public double getPpto_asignado() {
+		return ppto_asignado;
+	}
+
+	public void setPpto_asignado(double ppto_asignado) {
+		this.ppto_asignado = ppto_asignado;
+	}
+
+	public double getPorce_ppto_alcanzado() {
+		return porce_ppto_alcanzado;
+	}
+
+	public void setPorce_ppto_alcanzado(double porce_ppto_alcanzado) {
+		this.porce_ppto_alcanzado = porce_ppto_alcanzado;
+	}
+
+	public double getPpto_alcanzado() {
+		return ppto_alcanzado;
+	}
+
+	public void setPpto_alcanzado(double ppto_alcanzado) {
+		this.ppto_alcanzado = ppto_alcanzado;
+	}
+
+	public double getPpto_restante() {
+		return ppto_restante;
+	}
+
+	public void setPpto_restante(double ppto_restante) {
+		this.ppto_restante = ppto_restante;
+	}
 
 }
