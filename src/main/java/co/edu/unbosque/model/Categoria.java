e8541a7988b576dec923acb8b1ef84d725327886
TatianaPulido <tatiana_d25@hotmail.com>
2021-11-17 19:41:53 -0500

    Conf

---
+++ src/main/java/co/edu/unbosque/model/Categoria.java
@@ -0,0 +1,44 @@
+package co.edu.unbosque.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name = "categoria", nullable = false)
+	private String categoria;
+
+	public Categoria(long id, String categoria) {
+		super();
+		this.id = id;
+		this.categoria = categoria;
+	}
+public Categoria() {
+	
+}
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+}
---
+++ src/main/java/co/edu/unbosque/model/Concepto.java
@@ -0,0 +1,55 @@
+package co.edu.unbosque.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "concepto")
+public class Concepto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name = "id_categoria", nullable = false)
+	private long id_categoria;
+	@Column(name = "concepto", nullable = false)
+	private String concepto;
+
+	public Concepto(long id, long id_categoria, String concepto) {
+		super();
+		this.id = id;
+		this.id_categoria = id_categoria;
+		this.concepto = concepto;
+	}
+	public Concepto() {
+		
+	}
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId_categoria() {
+		return id_categoria;
+	}
+
+	public void setId_categoria(long id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+
+	public String getConcepto() {
+		return concepto;
+	}
+
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+
+}
