56e864c345a187eb877842f9da06913e9fc7a224
TatianaPulido <tatiana_d25@hotmail.com>
2021-11-18 16:02:28 -0500

    CRUD presupuesto terminado

--- src/main/java/co/edu/unbosque/model/Concepto.java
+++ src/main/java/co/edu/unbosque/model/Concepto.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Concepto {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@Column(name = "id_categoria", nullable = false)
 	private long id_categoria;
--- src/main/java/co/edu/unbosque/repository/PresupuestoVisRepository.java
+++ src/main/java/co/edu/unbosque/repository/PresupuestoVisRepository.java
@@ -9,7 +9,7 @@ import co.edu.unbosque.model.PresupuestoVis;
 
 public interface PresupuestoVisRepository extends JpaRepository<PresupuestoVis, Long> {
 
-	@Query(value = "Select p.id, a.categoria, c.concepto,p.anio,p.ppto_asignado,p.porce_ppto_alcanzado, p.ppto_alcanzado, p.ppto_restante from presupuesto p,concepto c, categoria a Where p.id_concepto=c.id and c.id_categoria=a.id", nativeQuery = true)
+	@Query(value = "select p.id, a.categoria, c.concepto,p.anio,p.ppto_asignado,p.porce_ppto_alcanzado, p.ppto_alcanzado, p.ppto_restante from presupuesto p,concepto c, categoria a Where p.id_concepto=c.id and c.id_categoria=a.id order by p.anio, a.categoria", nativeQuery = true)
 	public List<PresupuestoVis> obtenerPresupuesto();
 
 }
