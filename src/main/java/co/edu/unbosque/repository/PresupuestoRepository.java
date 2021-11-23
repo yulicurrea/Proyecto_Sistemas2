0cabaf31d513ec00f22de1bdcff16c900d679188
Charli199 <61799577+Charli199@users.noreply.github.com>
2021-11-23 18:12:09 -0500

    Update PresupuestoRepository.java

--- src/main/java/co/edu/unbosque/repository/PresupuestoRepository.java
+++ src/main/java/co/edu/unbosque/repository/PresupuestoRepository.java
@@ -1,5 +1,7 @@
 package co.edu.unbosque.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,5 +12,7 @@ public interface PresupuestoRepository extends JpaRepository<Presupuesto, Long>{
 	
 	@Query(value = "select count(*) from presupuesto where id_concepto=:id and anio=:anio_pre",nativeQuery = true)
 	long validarAsignacionPresupuesto(@Param("id") long id,@Param("anio_pre") int anio_pre);
+	
+	
 
 }
